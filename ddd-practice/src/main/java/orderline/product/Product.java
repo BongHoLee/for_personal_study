2d40068c47769b3b264206c9e4780d928e5dca86
bongholee <leebongho9204@gmail.com>
2021-08-26 20:19:19 +0900

    상품 구현 완료

--- ddd-practice/src/main/java/orderline/product/Product.java
+++ ddd-practice/src/main/java/orderline/product/Product.java
@@ -1,4 +1,19 @@
 package orderline.product;
 
 public class Product {
+    private final String productName;
+    private final String productCode;
+    private final Volume volume;
+    private final Price price;
+
+    public Product(String productName, String productCode, Volume volume, Price price) {
+        this.productName = productName;
+        this.productCode = productCode;
+        this.volume = volume;
+        this.price = price;
+    }
+
+    public Price price() {
+        return Price.of(price.value() * volume.getVolume());
+    }
 }
--- ddd-practice/src/test/java/orderline/product/ProductTest.java
+++ ddd-practice/src/test/java/orderline/product/ProductTest.java
@@ -1,10 +1,32 @@
 package orderline.product;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProductTest {
 
     @Test
-    void createTest() {
+    void 생성_테스트() {
+        Product product = new Product(
+                "productName",
+                "productCode",
+                Volume.of(10),
+                Price.of(1000)
+        );
+    }
+
+    @Test
+    void 가격_반환_테스트() {
+        long vol = 10;
+        long price = 1000;
+
+        Product product = new Product(
+                "productName",
+                "productCode",
+                Volume.of(vol),
+                Price.of(price)
+        );
+
+        Assertions.assertThat(product.price().value()).isEqualTo(vol * price);
     }
 }
