b19563a08a21222c48225379bc7a45ce758e3bf7
bongholee <leebongho9204@gmail.com>
2021-08-27 20:19:19 +0900

    주문 항목 계산 구현 완료

--- ddd-practice/src/main/java/orderline/OrderLine.java
+++ ddd-practice/src/main/java/orderline/OrderLine.java
@@ -1,8 +1,22 @@
 package orderline;
 
+import java.util.List;
+import orderline.product.Price;
+import orderline.product.Product;
+
 public class OrderLine {
+    private final List<Product> products;
 
-    public void price() {
+    public OrderLine(List<Product> products) {
+        this.products = products;
+    }
 
+    public Price price() {
+        Price price = Price.of(0);
+        for (Product product : products) {
+            price = price.sumWith(product.price());
+        }
+
+        return price;
     }
 }
--- ddd-practice/src/test/java/orderline/OrderLineTest.java
+++ ddd-practice/src/test/java/orderline/OrderLineTest.java
@@ -1,11 +1,36 @@
 package orderline;
 
+import java.util.Arrays;
+import orderline.product.Price;
+import orderline.product.Product;
+import orderline.product.Volume;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OrderLineTest {
 
     @Test
     void createOrderLineTest() {
+        new OrderLine(
+                Arrays.asList(
+                        new Product("pr1", "prn1", Volume.of(10), Price.of(2)),
+                        new Product("pr2", "prn2", Volume.of(11), Price.of(3)),
+                        new Product("pr3", "prn3", Volume.of(12), Price.of(4)),
+                        new Product("pr4", "prn4", Volume.of(13), Price.of(5))
+                )
+        );
+    }
+
+    @Test
+    void 가격_계산_태스트() {
+        OrderLine orderLine = new OrderLine(
+                Arrays.asList(
+                        new Product("pr1", "prn1", Volume.of(10), Price.of(2)),
+                        new Product("pr2", "prn2", Volume.of(11), Price.of(3))
+                )
+        );
+
+        Assertions.assertThat(orderLine.price().value()).isEqualTo(10*2 + 11*3);
     }
 
 }
