a4d346dbd5d84c8ee5995a8ee674f8f356083496
leebongho <bh9204@naver.com>
2021-09-28 15:39:03 +0900

    Spring-cache 사용 환경 설정

---
+++ Spring-Cache-Test/src/main/java/com/example/springcachetest/SpringCacheTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.springcachetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheTestApplication.class, args);
+    }
+}
---
+++ Spring-Cache-Test/src/test/kotlin/com/example/springcachetest/SpringCacheTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springcachetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCacheTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
