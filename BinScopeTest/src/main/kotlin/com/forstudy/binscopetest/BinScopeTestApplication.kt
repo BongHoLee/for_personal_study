3a842e91fce3c2986c087877c74b56dbcdd1da76
leebongho <bh9204@naver.com>
2021-10-20 08:42:36 +0900

    빈 스코프 테스트 프로젝트 init

---
+++ BinScopeTest/src/main/kotlin/com/forstudy/binscopetest/BinScopeTestApplication.kt
@@ -0,0 +1,11 @@
+package com.forstudy.binscopetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BinScopeTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<BinScopeTestApplication>(*args)
+}
---
+++ BinScopeTest/src/test/kotlin/com/forstudy/binscopetest/BinScopeTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.forstudy.binscopetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BinScopeTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
