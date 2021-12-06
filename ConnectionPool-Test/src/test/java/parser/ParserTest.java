d20d2545adb051d5abded38e6bb814f79a749bcd
leebongho <bh9204@naver.com>
2021-12-06 16:26:11 +0900

    sql parsing getValue test

--- ConnectionPool-Test/src/test/java/parser/ParserTest.java
+++ ConnectionPool-Test/src/test/java/parser/ParserTest.java
@@ -18,21 +18,38 @@ public class ParserTest {
     @Test
     @DisplayName("패턴 매칭 테스트")
     public void Pattern_매칭_테스트() {
-        String script = "SELECT A, B, C FROM table WHERE ID=#{ID} AND AGE=#{AGE}";
+        String script = "SELECT A, B, C FROM table WHERE ID=#{ID} AND AGE=#{AGE} AND HEIGHT=#{HEIGHT}";
         Matcher matcher = pattern.matcher(script);
 
         Map<String, Object> map = new HashMap<>();
         map.put("ID", "LEEBONGHO");
         map.put("AGE", 30);
+        map.put("HEIGHT", 170.1);
 
         while (matcher.find()) {
             String fParam = matcher.group();
             String fId 		= fParam.replaceAll("[\\$|\\#]\\{","").replaceAll("\\}","");
 
-            script = script.replace(fParam, (CharSequence) map.get(fId));
+            script = script.replace(fParam, getValue(map, fId));
             System.out.println(script);
 
         }
+    }
+
+    private String getValue(Map<String, Object> map, String key) {
+        if (map.containsKey(key)) {
+            String result = String.valueOf(map.get(key));
+
+            if (map.get(key) instanceof Number) {
+                return result;
+            }
+
+            if (map.get(key) instanceof String) {
+                return "'" + result + "'";
+            }
+        }
+
+        return "";
     }
 
 }
