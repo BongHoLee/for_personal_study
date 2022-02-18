801f5a156b93aec8510417c3fbb12a02a45f2a42
bongholee <leebongho9204@gmail.com>
2022-02-18 20:19:19 +0900

    WebIgnore을 이용한 정적 리소스 Security Filter 보안 해제

--- spring-security-demo/src/main/java/io/security/corespringsecurity/config/SecurityConfig.java
+++ spring-security-demo/src/main/java/io/security/corespringsecurity/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -33,6 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("admin").password(password).roles("USER", "MANAGER", "ADMIN");
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
