293536117f2dfdbcfc175538176d9f3d0827b8cc
bongholee <leebongho9204@gmail.com>
2022-04-04 20:19:19 +0900

    print-prime 프로젝트 생성 - 함수 쪼개기 연습 2

---
+++ print-prime/src/test/java/function/PrintPrimesTest.java
@@ -0,0 +1,13 @@
+package function;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class PrintPrimesTest {
+    @Test
+    public void
+    makeSureMatchesGold() throws IOException {
+        PrintPrimes.main(new String[0]);
+    }
+}
