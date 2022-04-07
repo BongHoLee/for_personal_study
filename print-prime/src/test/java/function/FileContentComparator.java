af7d5ebfa43dd030676471915e033d41ba487a28
bongholee <leebongho9204@gmail.com>
2022-04-07 21:29:42 +0900

    0. Add Characterization Test - 파일 비교 테스트 추가.

--- print-prime/src/test/java/function/FileContentComparator.java
+++ print-prime/src/test/java/function/FileContentComparator.java
@@ -1,15 +1,42 @@
 package function;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FileContentComparator {
-    public void compare() {
+
+    private PrintStream out;
+
+    @Before
+    public void setUp() throws FileNotFoundException {
+        out = System.out;
+        System.setOut(new PrintStream(new FileOutputStream("lead")));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(out);
+        new File("lead").delete();
+    }
+
+
+    @Test
+    public void makeSureMatchesGold() {
+        PrintPrimes.main(new String[0]);
+
         try {
             BufferedReader lead = new BufferedReader(new FileReader("lead"));
             BufferedReader gold = new BufferedReader(new FileReader("src/test/java/function/gold"));
+
             String line;
             while ((line = gold.readLine()) != null)
                 assertEquals(line, lead.readLine());
