2d6c083d814d723d04093cde2b21c4244bda5874
bongholee <leebongho9204@gmail.com>
2022-05-05 20:19:19 +0900

    feat(Entities): 기본 엔티티 구현 1차

--- JPA_LECTURE/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ JPA_LECTURE/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.Embeddable;
+import lombok.Getter;
 
 // JPA의 내장 타입
 // DDD에서의 Value type
 @Embeddable
+@Getter
 public class Address {
 
     private String city;
--- JPA_LECTURE/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA_LECTURE/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,11 +1,18 @@
 package jpabook.jpashop.domain;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,9 +27,21 @@ public class Order {
     private Long id;
 
     // Order와 Member는 N:1 관계
-    // N쪽이 Orders 이므로 F.K가 있다.
+    // N쪽이 Order 이므로 F.K가 있다.
     // 양방향 관계라면 F.K가 있는 쪽이 "연관관계의 주인"
     @ManyToOne
     @JoinColumn(name = "member_id")     // F.K 명이 member_id
     private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 }
