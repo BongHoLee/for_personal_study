85d3f4b4f50efd8e72c9c635061388350a330bfb
bongholee <leebongho9204@gmail.com>
2022-05-19 22:19:26 +0900

    init project

---
+++ ddd-start2-practice/src/main/java/com/myshop/DddStart2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStart2PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddStart2PracticeApplication.class, args);
+    }
+
+}
