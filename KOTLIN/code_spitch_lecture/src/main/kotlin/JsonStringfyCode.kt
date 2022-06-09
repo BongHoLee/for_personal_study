263afbd2ce639e68e1a597082b161c118e536b26
bongholee <leebongho9204@gmail.com>
2022-06-09 21:55:27 +0900

    joinTo 확장함수 반영

--- KOTLIN/code_spitch_lecture/src/main/kotlin/JsonStringfyCode.kt
+++ KOTLIN/code_spitch_lecture/src/main/kotlin/JsonStringfyCode.kt
@@ -2,17 +2,12 @@ import kotlin.reflect.KProperty
 
 // T 타입의 객체를 JSON 문자열 형태로 반환하는 함수
 fun <T: Any> stringify(target: T): String {
-    val builder = StringBuilder()
-    builder.append("{")
 
-    target::class.members.filterIsInstance<KProperty<*>>().forEach { it ->
-        builder.append(jsonString(it.name), ":")
-        val value = it.getter.call(target)
-        builder.append(if (value is String) jsonString(value) else value, ",")
-    }
-
-    builder.append("}")
-    return "$builder"
+    return target::class.members.filterIsInstance<KProperty<*>>()
+        .joinTo(StringBuilder(), ",", "{", "}") {
+            val value = it.getter.call(target)
+            "${jsonString(it.name)} : ${if(value is String) jsonString(value) else value}"
+        }.toString()
 }
 
 private fun jsonString(value: String) = """"${value.replace("\"", "\\\"")}""""
