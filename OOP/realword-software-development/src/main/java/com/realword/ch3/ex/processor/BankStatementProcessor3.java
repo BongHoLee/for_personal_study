f457b5b07acdb18d5aff9e7041727d88f9d35d05
leebongho <leebongho9204@gmail.com>
2022-07-15 11:24:27 +0900

    Real-world software development 스터디

--- OOP/realword-software-development/src/main/java/com/realword/ch3/ex/processor/BankStatementProcessor3.java
+++ OOP/realword-software-development/src/main/java/com/realword/ch3/ex/processor/BankStatementProcessor3.java
@@ -2,7 +2,6 @@ package com.realword.ch3.ex.processor;
 
 import com.realword.ch3.ex.filter.BankTransactionFilter;
 import com.realword.ch3.ex.summarize.sum.BankTransactionSummarizer;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,34 +17,6 @@ public class BankStatementProcessor3 {
         double total = 0;
         for (BankTransaction bankTransaction : bankTransactions) {
             total = summarizer.summarize(total, bankTransaction);
-        }
-        return total;
-    }
-    public double calculateTotalAmount() {
-        double total = 0;
-        for (BankTransaction bankTransaction : bankTransactions) {
-            total += bankTransaction.getAmount();
-        }
-        return total;
-    }
-
-    public double calculateTotalInMonth(final Month month) {
-        double total = 0;
-        for (BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDate().getMonth() == month) {
-
-                total += bankTransaction.getAmount();
-            }
-        }
-        return total;
-    }
-
-    public double calculateTotalForCategory(final String category) {
-        double total = 0;
-        for (BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDescription().equals(category)) {
-                total += bankTransaction.getAmount();
-            }
         }
         return total;
     }
