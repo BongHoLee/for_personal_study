ffcc3189fc8d095bc20b9c44f0af18e6194b0218
bongholee <leebongho9204@gmail.com>
2022-08-25 22:07:39 +0900

    update

---
+++ SPRING/spring-basic-kisun/src/main/java/app/BootApplication.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
